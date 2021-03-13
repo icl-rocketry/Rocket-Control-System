d43f33dcadc8405deb499764fa37daaa3d9bea3b
STELIOS1999 <74464052+STELIOS1999@users.noreply.github.com>
2021-03-13 15:47:10 +0200

    Add files via upload

---
+++ linearisation_control_system_rocket.m
@@ -0,0 +1,18 @@
+
+
+syms rho Sref Cd  W z m theta T 
+f1=W;
+
+f2=T*sin(theta)/m-0.5*rho*Sref*Cd*(W^2)*sin(theta)/m-9.81;
+
+A=[diff([f1;f2],z),diff([f1;f2],W)];
+
+B=diff([f1;f2],T);
+
+C=[1,0;0,1];
+
+D=[0;0];
+
+
+
+
