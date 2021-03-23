a17b311a4ebf6f0b8fbb6a47cf9b944baeff8a1c
STELIOS1999 <74464052+STELIOS1999@users.noreply.github.com>
2021-03-23 21:48:39 +0200

    Add files via upload

--- rocket_data_extraction.m
+++ rocket_data_extraction.m
@@ -1,23 +1,25 @@
 clear
 clc
 
-fileID=fopen('open_rocket_data.csv','r');
+fileID=fopen('open_rocket_data_corrected.csv','r');
 
-time(1:123)=0;
+time(1:123)=0; %in seconds
 
-altitude(1:123)=0;
+altitude(1:123)=0; %in meters
 
-Vertical_velocity(1:123)=0;
+Vertical_velocity(1:123)=0; %,m/s
 
-Mass(1:123)=0;
+pitchrate(1:123)=0;%degrees /s
+
+Mass(1:123)=0;%grams
 
 Cd(1:123)=0;
 
-T(1:123)=0;%temperature
+T(1:123)=0;%temperature in celcius
 
-P(1:123)=0;
+P(1:123)=0;% in mbar
 
-
+Sref=210.06*(10-2)^2; %reference area in m^2
 
 for i=1:123
     time(i)=fscanf(fileID,'%f',1);
@@ -26,6 +28,8 @@ for i=1:123
 
     Vertical_velocity(i)=fscanf(fileID,'%f',1);
 
+   pitchrate(i)=fscanf(fileID,'%f',1);
+    
      Mass(i)=fscanf(fileID,'%f',1);
 
       Cd(i)=fscanf(fileID,'%f',1);
@@ -40,4 +44,30 @@ fclose(fileID);
 
 R=287;
 
+%conversion in SI units
+P=P*10^3;
+
+Mass=Mass/1000;
+
+T=T+273;
+
+pitchrate=pitchrate*pi/180;
+
+
+
+%density calculation
 rho=P./(R*T);
+
+
+%flight angle calculation
+dt=time(2:length(T))-time(1:length(T)-1);
+
+pitch_rate_average=(pitchrate(1:length(pitchrate)-1)+pitchrate(2:length(pitchrate)))/2;
+
+flight_angle(1:length(T))=0;
+
+for i=1:length(T)-1
+flight_angle(i+1)=flight_angle(i)+dt(i)*pitch_rate_average(i);
+end
+
+
